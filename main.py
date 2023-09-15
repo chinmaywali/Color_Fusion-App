36352fc3668611eaed048d50504b8b3880060947
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-09-15 14:00:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,115 @@
+
 import random
-
 from kivy.app import App
-from kivy.core.window import Window
-from kivy.graphics import Ellipse, Color, Line
-from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Ellipse, Color, Line
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
 
-Window.clearcolor = (1, 1, 1, 1)  # r,g,b,alpha
-
+Window.clearcolor = (1, 1, 1, 1)  # White background color
 
 class PaintWindow(Widget):
     def on_touch_down(self, touch):
-        colorR = random.randint(0, 255)
-        colorG = random.randint(0, 255)
-        colorB = random.randint(0, 255)
+        # Randomize colors for drawing
+        colorR = random.random()
+        colorG = random.random()
+        colorB = random.random()
 
-        self.canvas.add(Color(rgb=(colorR / 255.0, colorG / 255.0, colorB / 255.0)))
-        # print(touch) x and y coordinte of touch
-        d = 50
-        self.canvas.add(Line(size=(d, d), pos=(touch.x - d / 2, touch.y - d / 2)))
-        touch.ud['line'] = Line(points=(touch.x, touch.y))
-        self.canvas.add(touch.ud['line'])
+        with self.canvas:
+            Color(rgb=(colorR, colorG, colorB))
+            # Draw a circle for the paintbrush
+            d = 5
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=2)
 
     def on_touch_move(self, touch):
+        # Continue drawing when moving
         touch.ud['line'].points += [touch.x, touch.y]
 
-
-# root window = paint window + button
-class ColorFusionApp(App):
+class PaintApp(App):
     def build(self):
-        rootWindow = Widget()
         self.painter = PaintWindow()
-        clearnBtn = Button(text='Clear' )  # pos = (0 ,600) , size = (100, 100 ) )
-        clearnBtn.bind(on_release=self.clear_canvas)
-        rootWindow.add_widget(self.painter)
-        rootWindow.add_widget(clearnBtn)
+        self.root = self.create_ui()
+        return self.root
 
-        return rootWindow
+    def create_ui(self):
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
 
-    def clear_canvas(self, obj):
+        top_bar = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50)
+        clear_button = Button(text='Clear', size_hint=(None, 1))
+        clear_button.bind(on_release=self.clear_canvas)
+        save_button = Button(text='Save', size_hint=(None, 1))
+        save_button.bind(on_release=self.save_canvas)
+        load_button = Button(text='Load', size_hint=(None, 1))
+        load_button.bind(on_release=self.load_canvas)
+
+        top_bar.add_widget(clear_button)
+        top_bar.add_widget(save_button)
+        top_bar.add_widget(load_button)
+
+        layout.add_widget(top_bar)
+        layout.add_widget(self.painter)
+
+        return layout
+
+    def clear_canvas(self, instance):
+        # Clear the canvas by removing all instructions
         self.painter.canvas.clear()
 
+    def save_canvas(self, instance):
+        # Create a popup for saving the canvas
+        popup_layout = BoxLayout(orientation='vertical')
+        filename_input = TextInput(hint_text='Enter filename', multiline=False, size_hint=(1, None), height=30)
+        save_button = Button(text='Save', size_hint=(1, None), height=50)
 
-ColorFusionApp().run()
+        popup_layout.add_widget(filename_input)
+        popup_layout.add_widget(save_button)
 
+        popup_content = GridLayout(cols=1)
+        popup_content.add_widget(popup_layout)
+
+        popup = Popup(title='Save Canvas', content=popup_content, size_hint=(None, None), size=(300, 150))
+
+        def save_callback(instance):
+            filename = filename_input.text + ".png"
+            self.painter.export_to_png(filename)
+            popup.dismiss()
+
+        save_button.bind(on_release=save_callback)
+        popup.open()
+
+    def load_canvas(self, instance):
+        # Create a popup for loading a canvas
+        popup_layout = BoxLayout(orientation='vertical')
+        file_chooser = FileChooserIconView()
+        file_chooser.path = "./"
+        load_button = Button(text='Load', size_hint=(1, None), height=50)
+
+        popup_layout.add_widget(file_chooser)
+        popup_layout.add_widget(load_button)
+
+        popup_content = GridLayout(cols=1)
+        popup_content.add_widget(popup_layout)
+
+        popup = Popup(title='Load Canvas', content=popup_content, size_hint=(None, None), size=(400, 400))
+
+        def load_callback(instance):
+            filename = file_chooser.selection and file_chooser.selection[0] or ""
+            if filename:
+                self.painter.canvas.clear()
+                self.painter.add_widget(Image(source=filename))
+            popup.dismiss()
+
+        load_button.bind(on_release=load_callback)
+        popup.open()
+
+if __name__ == '__main__':
+    PaintApp().run()
